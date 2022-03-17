9c7db2ea8b48ac3e9e425fb812956a17fd48d507
otavio <101368339+otaviosz@users.noreply.github.com>
2022-03-17 22:39:23 +0000

    ts

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask('app')
 
 @app.route('/')
-def hello_world():
-  return '<h1>Minha primeira rota!</h1>'
+def index():
+  return render_template('index.html')
 
-app.run(host='0.0.0.0', port=8080)
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=8080)
