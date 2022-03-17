77d7171837d3f2c5ad92e6b1c80050217bc1e783
otavio <101368339+otaviosz@users.noreply.github.com>
2022-03-17 22:50:03 +0000

    correção

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_templates
 app = Flask('app')
 
 @app.route('/')
-def hello_world():
-  return '<h1>Minha primeira rota!</h1>'
-
-app.run(host='0.0.0.0', port=8080)
+def index():
+  return render_templates('index.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
